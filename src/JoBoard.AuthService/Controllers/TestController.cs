d87dacfba9b6a19ba68d2ab8ec4d8bef7ccd4cc2
Vlad <unbroken.stargazer@gmail.com>
2024-01-04 21:11:18 +0300

    add docker

---
+++ src/JoBoard.AuthService/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JoBoard.AuthService.Controllers;
+
+[Route("test")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
--- src/JoBoard.AuthService/Program.cs
+++ src/JoBoard.AuthService/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
